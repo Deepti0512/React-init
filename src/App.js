bdd62be283a55f4d168e0798439aec4cc3eb5df2
Deepti <deeptimittal0512@gmail.com>
2023-05-03 11:43:33 +0530

    revert to previous state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import UserFinder from './components/UserFinder';
-import UsersContext from './store/users-context';
+import React from "react";
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
-
-function App() {
-  const usersContext = {
-    users: DUMMY_USERS
-  }
-
-  return (
-    <UsersContext.Provider value={usersContext}>
-      <UserFinder />
-    </UsersContext.Provider>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
